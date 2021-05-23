1cc1f6c12b5537ee174c0398abf236caf9fa7fa6
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-05-23 09:42:03 +0200

    Chec out button

--- WindowsFormsApp1/Form5.Designer.cs
+++ WindowsFormsApp1/Form5.Designer.cs
@@ -73,9 +73,9 @@ namespace WindowsFormsApp1
             // 
             this.button1.Location = new System.Drawing.Point(343, 366);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 51);
+            this.button1.Size = new System.Drawing.Size(123, 53);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Make Payment";
+            this.button1.Text = "Check-Out";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
