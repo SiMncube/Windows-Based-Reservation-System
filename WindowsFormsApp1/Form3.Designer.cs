7e28d0fdb1b6bf5b946c291eddd61bfa867ddc32
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-05-25 07:24:48 +0200

    Gallery

--- WindowsFormsApp1/Form3.Designer.cs
+++ WindowsFormsApp1/Form3.Designer.cs
@@ -29,18 +29,25 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.pictureBox4);
             this.panel1.Location = new System.Drawing.Point(124, 82);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1066, 554);
@@ -54,6 +61,7 @@ namespace WindowsFormsApp1
             this.pictureBox2.Size = new System.Drawing.Size(89, 85);
             this.pictureBox2.TabIndex = 25;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
             // 
             // pictureBox1
             // 
@@ -73,6 +81,29 @@ namespace WindowsFormsApp1
             this.pictureBox3.Size = new System.Drawing.Size(86, 85);
             this.pictureBox3.TabIndex = 26;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Outside1.jpg");
+            this.imageList1.Images.SetKeyName(1, "Outside2.jpg");
+            this.imageList1.Images.SetKeyName(2, "DR1.jpg");
+            this.imageList1.Images.SetKeyName(3, "DR2.jpg");
+            this.imageList1.Images.SetKeyName(4, "DR3.jpg");
+            this.imageList1.Images.SetKeyName(5, "SR1.jpg");
+            this.imageList1.Images.SetKeyName(6, "SR2.jpg");
+            this.imageList1.Images.SetKeyName(7, "SR3.jpg");
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(111, 92);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(844, 370);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
             // 
             // Form3
             // 
@@ -86,9 +117,11 @@ namespace WindowsFormsApp1
             this.Name = "Form3";
             this.Text = "Form3";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -98,5 +131,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
--- WindowsFormsApp1/Form3.cs
+++ WindowsFormsApp1/Form3.cs
@@ -12,14 +12,41 @@ namespace WindowsFormsApp1
 {
     public partial class Form3 : Form
     {
+        int count = 0;
         public Form3()
         {
             InitializeComponent();
+            pictureBox4.Image = imageList1.Images[count];
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+            if (count > 0)
+            {
+                count--;
+            }
+            else
+            {
+                count = 6;
+            }
+            pictureBox4.Image = imageList1.Images[count];
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if(count < 6)
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+            }
+            pictureBox4.Image = imageList1.Images[count];
         }
     }
 }
