ee47903cc9b1d3ef147855cd8eeb40c2d43c1568
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-06-10 22:48:46 +0200

    ..

--- WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -30,6 +30,11 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
             this.label9 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -66,11 +71,6 @@ namespace WindowsFormsApp1
             this.panel3 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
@@ -114,10 +114,66 @@ namespace WindowsFormsApp1
             this.panel1.Controls.Add(this.label21);
             this.panel1.Controls.Add(this.label20);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(497, 213);
+            this.panel1.Location = new System.Drawing.Point(475, 147);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(422, 461);
             this.panel1.TabIndex = 1;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(360, 70);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(18, 25);
+            this.label14.TabIndex = 67;
+            this.label14.Text = ".";
+            this.label14.Visible = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(336, 70);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(18, 25);
+            this.label13.TabIndex = 66;
+            this.label13.Text = ".";
+            this.label13.Visible = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(312, 70);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(18, 25);
+            this.label12.TabIndex = 65;
+            this.label12.Text = ".";
+            this.label12.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(288, 70);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(18, 25);
+            this.label11.TabIndex = 64;
+            this.label11.Text = ".";
+            this.label11.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Gray;
+            this.label8.Location = new System.Drawing.Point(43, 77);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(245, 16);
+            this.label8.TabIndex = 62;
+            this.label8.Text = "Sign up successfull redirecting to homepage";
+            this.label8.Visible = false;
             // 
             // panel4
             // 
@@ -462,7 +518,7 @@ namespace WindowsFormsApp1
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(585, 66);
+            this.label2.Location = new System.Drawing.Point(548, 66);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(250, 18);
             this.label2.TabIndex = 18;
@@ -487,7 +543,7 @@ namespace WindowsFormsApp1
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Lucida Fax", 39.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label7.Location = new System.Drawing.Point(468, 5);
+            this.label7.Location = new System.Drawing.Point(431, 5);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(483, 61);
             this.label7.TabIndex = 17;
@@ -516,7 +572,7 @@ namespace WindowsFormsApp1
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(513, 9);
+            this.label4.Location = new System.Drawing.Point(480, 9);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(414, 18);
             this.label4.TabIndex = 41;
@@ -529,62 +585,6 @@ namespace WindowsFormsApp1
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(0, 13);
             this.label5.TabIndex = 0;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.Gray;
-            this.label8.Location = new System.Drawing.Point(43, 77);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(245, 16);
-            this.label8.TabIndex = 62;
-            this.label8.Text = "Sign up successfull redirecting to homepage";
-            this.label8.Visible = false;
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(288, 70);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(18, 25);
-            this.label11.TabIndex = 64;
-            this.label11.Text = ".";
-            this.label11.Visible = false;
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(312, 70);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(18, 25);
-            this.label12.TabIndex = 65;
-            this.label12.Text = ".";
-            this.label12.Visible = false;
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(336, 70);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(18, 25);
-            this.label13.TabIndex = 66;
-            this.label13.Text = ".";
-            this.label13.Visible = false;
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.Location = new System.Drawing.Point(360, 70);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(18, 25);
-            this.label14.TabIndex = 67;
-            this.label14.Text = ".";
-            this.label14.Visible = false;
             // 
             // Form2
             // 
