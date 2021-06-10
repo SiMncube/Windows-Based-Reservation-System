c96255dcd4af37831c2d80426188f08b7f85e632
SiMncube <mncubepraiseworth@gmail.com>
2021-06-10 11:49:26 +0200

    Forgot password update

--- WindowsFormsApp1/Form10.Designer.cs
+++ WindowsFormsApp1/Form10.Designer.cs
@@ -54,9 +54,9 @@ namespace WindowsFormsApp1
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(615, 211);
+            this.groupBox1.Location = new System.Drawing.Point(455, 196);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(395, 300);
+            this.groupBox1.Size = new System.Drawing.Size(407, 307);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = ".";
@@ -87,7 +87,7 @@ namespace WindowsFormsApp1
             this.button2.BackColor = System.Drawing.Color.SeaGreen;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(6, 252);
+            this.button2.Location = new System.Drawing.Point(20, 252);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(113, 42);
             this.button2.TabIndex = 4;
@@ -166,6 +166,7 @@ namespace WindowsFormsApp1
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.WhatsApp_Image_2021_06_02_at_15_53_29;
             this.ClientSize = new System.Drawing.Size(1370, 749);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
