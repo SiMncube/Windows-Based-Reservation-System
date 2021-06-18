934c337a4e50a0d13e7557e8ab1c08144477d2f0
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-06-18 18:34:58 +0200

    ...

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -21,6 +21,7 @@ namespace WindowsFormsApp1
 
         private void button3_Click(object sender, EventArgs e)
         {
+
             if (loginisValid())
             {
                 if(userNameIsCorrect() && userPasswordIsCorrect())
