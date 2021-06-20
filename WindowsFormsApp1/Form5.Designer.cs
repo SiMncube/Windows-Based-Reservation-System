53578eeff13d61bcd881c127babf0aaa38daf0b6
SihleNxasane <sinxasane@gmail.com>
2021-06-21 00:12:23 +0200

    ....

--- WindowsFormsApp1/Form5.Designer.cs
+++ WindowsFormsApp1/Form5.Designer.cs
@@ -217,7 +217,7 @@ namespace WindowsFormsApp1
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(126, 33);
             this.button4.TabIndex = 35;
-            this.button4.Text = "Save changes";
+            this.button4.Text = "Save";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
