30e9ab22c3a1618e2024efd4ff0066cb5694c6e6
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-06-21 01:12:03 +0200

    ...

--- WindowsFormsApp1/confirmBookingForm.Designer.cs
+++ WindowsFormsApp1/confirmBookingForm.Designer.cs
@@ -329,11 +329,11 @@ namespace WindowsFormsApp1
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.label2.Location = new System.Drawing.Point(48, 42);
+            this.label2.Location = new System.Drawing.Point(49, 32);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(208, 16);
+            this.label2.Size = new System.Drawing.Size(101, 16);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Your reservation is now confirmed";
+            this.label2.Text = "Booking details";
             // 
             // label1
             // 
