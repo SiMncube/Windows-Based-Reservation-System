621585dec838f30816b6fae3a37e0e4635fd34b3
SihleNxasane <sinxasane@gmail.com>
2021-06-20 21:55:23 +0200

    ....

--- WindowsFormsApp1/cancelBookingForm.designer.cs
+++ WindowsFormsApp1/cancelBookingForm.designer.cs
@@ -129,11 +129,11 @@ namespace WindowsFormsApp1
             this.button1.BackColor = System.Drawing.Color.DodgerBlue;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(43, 163);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(277, 50);
+            this.button1.Size = new System.Drawing.Size(277, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "Cancel Booking";
             this.button1.UseVisualStyleBackColor = false;
