ad64ca751ff12b3e69dfc9f7c8e89e3dd67d7e89
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-07-01 20:20:51 +0200

    .....

--- WindowsFormsApp1/modifyConfirm.Designer.cs
+++ WindowsFormsApp1/modifyConfirm.Designer.cs
@@ -385,7 +385,7 @@ namespace WindowsFormsApp1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 450);
+            this.ClientSize = new System.Drawing.Size(533, 448);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
