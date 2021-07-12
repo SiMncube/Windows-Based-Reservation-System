a084bd3e5c9dfb2733c9c246fd79f303bb741c5e
Bandile Zwane <217021087@stu.ukzn.ac.za>
2021-07-12 15:03:33 +0200

    Font and cancellation form

--- WindowsFormsApp1/CancelbookingForm.designer.cs
+++ WindowsFormsApp1/CancelbookingForm.designer.cs
@@ -85,33 +85,37 @@ namespace WindowsFormsApp1
             // 
             // comboBox1
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "Change of mind",
-            "Choose another BnB",
-            "Not happy with the booking App",
-            "Non of the above"});
+            "Found a different accommodation option",
+            "Unable to travel due to Coronavirus (Covid-19 ) restrictions",
+            "Change of dates or destinations",
+            "Change in the number or needs of travelers",
+            "Made booking for the same dates- Cancelled the ones I don\'t need",
+            "Personal reasons",
+            "Trip called off",
+            "None of the above"});
             this.comboBox1.Location = new System.Drawing.Point(43, 117);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(277, 28);
+            this.comboBox1.Size = new System.Drawing.Size(277, 26);
             this.comboBox1.TabIndex = 5;
             this.comboBox1.Text = "Reason";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.GrayText;
             this.label3.Location = new System.Drawing.Point(39, 94);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(183, 20);
+            this.label3.Size = new System.Drawing.Size(190, 18);
             this.label3.TabIndex = 4;
             this.label3.Text = "Reason for cancelling";
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(43, 58);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(277, 26);
@@ -120,11 +124,11 @@ namespace WindowsFormsApp1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
             this.label1.Location = new System.Drawing.Point(39, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 23);
+            this.label1.Size = new System.Drawing.Size(211, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "Booking Reference *";
             // 
@@ -133,7 +137,7 @@ namespace WindowsFormsApp1
             this.button1.BackColor = System.Drawing.Color.DodgerBlue;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(43, 163);
             this.button1.Name = "button1";
@@ -161,11 +165,11 @@ namespace WindowsFormsApp1
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.label6.Location = new System.Drawing.Point(50, 53);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(203, 27);
+            this.label6.Size = new System.Drawing.Size(212, 24);
             this.label6.TabIndex = 0;
             this.label6.Text = "Refund Amount  : ";
             this.label6.Visible = false;
@@ -173,11 +177,11 @@ namespace WindowsFormsApp1
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Bold);
             this.label8.ForeColor = System.Drawing.Color.DarkViolet;
             this.label8.Location = new System.Drawing.Point(51, 53);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(256, 23);
+            this.label8.Size = new System.Drawing.Size(247, 24);
             this.label8.TabIndex = 8;
             this.label8.Text = "Booking was not paid";
             this.label8.Visible = false;
@@ -185,11 +189,11 @@ namespace WindowsFormsApp1
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Lucida Fax", 1.75F, System.Drawing.FontStyle.Bold);
             this.label7.ForeColor = System.Drawing.Color.DarkViolet;
             this.label7.Location = new System.Drawing.Point(36, 53);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(316, 23);
+            this.label7.Size = new System.Drawing.Size(46, 4);
             this.label7.TabIndex = 6;
             this.label7.Text = "Booking Ref does not exist";
             this.label7.Visible = false;
@@ -197,11 +201,11 @@ namespace WindowsFormsApp1
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Bold);
             this.label5.ForeColor = System.Drawing.Color.DarkViolet;
             this.label5.Location = new System.Drawing.Point(11, 53);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(341, 23);
+            this.label5.Size = new System.Drawing.Size(325, 24);
             this.label5.TabIndex = 7;
             this.label5.Text = "Booking is already cancelled";
             this.label5.Visible = false;
@@ -209,11 +213,11 @@ namespace WindowsFormsApp1
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Bold);
             this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label4.Location = new System.Drawing.Point(51, 30);
+            this.label4.Location = new System.Drawing.Point(50, 53);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(244, 23);
+            this.label4.Size = new System.Drawing.Size(240, 24);
             this.label4.TabIndex = 4;
             this.label4.Text = "Booking is Cancelled";
             this.label4.Visible = false;
@@ -266,11 +270,11 @@ namespace WindowsFormsApp1
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.Color.White;
             this.label10.Location = new System.Drawing.Point(11, 66);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(43, 16);
+            this.label10.Size = new System.Drawing.Size(38, 15);
             this.label10.TabIndex = 41;
             this.label10.Text = "Back";
             // 
