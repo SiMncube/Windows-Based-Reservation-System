e7f9cf7f9e73b95cf0611582b8bb9d6c02476d35
SiMncube <mncubepraiseworth@gmail.com>
2021-08-29 11:52:40 +0200

    Update on reports

---
+++ WindowsFormsApp1/reportViewer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Payment_Report : Form
+    {
+        public Payment_Report()
+        {
+            InitializeComponent();
+        }
+    }
+}
