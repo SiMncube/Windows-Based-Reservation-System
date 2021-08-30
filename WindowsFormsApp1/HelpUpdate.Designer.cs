832029c5b02576f58c2b3304514963fd58f77d07
Bandile Zwane <217021087@stu.ukzn.ac.za>
2021-08-30 15:35:12 +0200

    ,.,.

--- WindowsFormsApp1/HelpUpdate.Designer.cs
+++ WindowsFormsApp1/HelpUpdate.Designer.cs
@@ -31,6 +31,11 @@ namespace WindowsFormsApp1
         {
             this.button2 = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
@@ -58,11 +63,63 @@ namespace WindowsFormsApp1
             this.label10.TabIndex = 43;
             this.label10.Text = "Back";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 26);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "1. In \"Update customer details\", search for customer profile using name, surname," +
+    " \r\n    email or ID number.\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(371, 26);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "2. Double click on the left hand column next to the customer\'s email address. \r\n " +
+    "   Once row has been highlighted, click \"Confirm\".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 13);
+            this.label3.TabIndex = 46;
+            this.label3.Text = "3. Customer details should appear below.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(273, 13);
+            this.label4.TabIndex = 47;
+            this.label4.Text = "4. Once new details have been entered, click \"Update\".";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(192, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(192, 13);
+            this.label5.TabIndex = 48;
+            this.label5.Text = "5. Customer details have been updated";
+            // 
             // HelpUpdate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.button2);
             this.Name = "HelpUpdate";
@@ -76,5 +133,10 @@ namespace WindowsFormsApp1
 
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
