91997444fb21091427e66e2310740a665585f6bd
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-09-08 20:46:44 +0200

    ...

--- WindowsFormsApp1/confirmUpdateCust.Designer.cs
+++ WindowsFormsApp1/confirmUpdateCust.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsFormsApp1
 {
-    partial class confirmCust
+    partial class confirmUpdateCust
     {
         /// <summary>
         /// Required designer variable.
@@ -29,26 +29,11 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label28 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.DodgerBlue;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(216, 160);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 43);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "OK";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // panel2
             // 
@@ -57,8 +42,8 @@ namespace WindowsFormsApp1
             this.panel2.Controls.Add(this.label28);
             this.panel2.Location = new System.Drawing.Point(12, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(540, 142);
-            this.panel2.TabIndex = 9;
+            this.panel2.Size = new System.Drawing.Size(518, 142);
+            this.panel2.TabIndex = 11;
             // 
             // label28
             // 
@@ -67,23 +52,38 @@ namespace WindowsFormsApp1
             this.label28.ForeColor = System.Drawing.Color.White;
             this.label28.Location = new System.Drawing.Point(3, 49);
             this.label28.Name = "label28";
-            this.label28.Size = new System.Drawing.Size(530, 48);
+            this.label28.Size = new System.Drawing.Size(512, 48);
             this.label28.TabIndex = 0;
-            this.label28.Text = "The customers details have been Successfully updated.\r\nA confirmation email is se" +
-    "nt";
+            this.label28.Text = "The customer has been Successfully Registered.\r\nAn Email containing their loggin " +
+    "details has been sent";
             // 
-            // confirmCust
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(188, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 43);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // confirmUpdateCust
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(564, 218);
+            this.ClientSize = new System.Drawing.Size(545, 220);
             this.ControlBox = false;
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.button2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "confirmCust";
+            this.Name = "confirmUpdateCust";
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = " ";
             this.panel2.ResumeLayout(false);
@@ -93,8 +93,9 @@ namespace WindowsFormsApp1
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
+
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.Button button2;
     }
 }
