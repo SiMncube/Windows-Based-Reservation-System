6fce751594d7dfb120c92cdc0e44e205b5ea15e4
Bandile Zwane <217021087@stu.ukzn.ac.za>
2021-09-11 12:52:14 +0200

    Cancel Help

--- WindowsFormsApp1/HelpCancel.Designer.cs
+++ WindowsFormsApp1/HelpCancel.Designer.cs
@@ -33,10 +33,8 @@ namespace WindowsFormsApp1
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
@@ -65,7 +63,7 @@ namespace WindowsFormsApp1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(89, 76);
+            this.label1.Location = new System.Drawing.Point(89, 122);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(498, 75);
@@ -77,47 +75,37 @@ namespace WindowsFormsApp1
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(89, 347);
+            this.label2.Location = new System.Drawing.Point(89, 419);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(314, 75);
+            this.label2.Size = new System.Drawing.Size(490, 50);
             this.label2.TabIndex = 45;
-            this.label2.Text = "2. Double click on the left hand \r\n    column next to the\r\n    customer\'s email a" +
-    "ddress.";
+            this.label2.Text = "2. Double click on the left hand column next to the\r\n    customer\'s email address" +
+    ".";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(780, 76);
+            this.label3.Location = new System.Drawing.Point(780, 122);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(471, 50);
+            this.label3.Size = new System.Drawing.Size(471, 75);
             this.label3.TabIndex = 46;
-            this.label3.Text = "3. Booking you want to cancel should appear in \r\n    the \"Cancel booking\" table";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(780, 347);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(254, 25);
-            this.label4.TabIndex = 47;
-            this.label4.Text = "4. Click \"Cancel Booking\"";
+            this.label3.Text = "3. Booking you want to cancel should appear in \r\n    the table below. \r\n    Proce" +
+    "ed to click \"Cancel booking\"  button ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(780, 469);
+            this.label5.Location = new System.Drawing.Point(780, 419);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(269, 25);
+            this.label5.Size = new System.Drawing.Size(422, 50);
             this.label5.TabIndex = 48;
-            this.label5.Text = "5. Select \"Process Refund\"";
+            this.label5.Text = "4. Select \"Process Refund\" button to finish.\r\n    Booking will be cancelled.";
             // 
             // label6
             // 
@@ -130,20 +118,10 @@ namespace WindowsFormsApp1
             this.label6.TabIndex = 49;
             this.label6.Text = "6. Booking has been cancelled";
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(780, 704);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(313, 25);
-            this.label7.TabIndex = 50;
-            this.label7.Text = "6. Booking has been cancelled.";
-            // 
             // pictureBox5
             // 
             this.pictureBox5.Image = global::WindowsFormsApp1.Properties.Resources.Screenshot__320_;
-            this.pictureBox5.Location = new System.Drawing.Point(785, 497);
+            this.pictureBox5.Location = new System.Drawing.Point(785, 472);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(571, 195);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -154,7 +132,7 @@ namespace WindowsFormsApp1
             // pictureBox3
             // 
             this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.Screenshot__319_;
-            this.pictureBox3.Location = new System.Drawing.Point(785, 144);
+            this.pictureBox3.Location = new System.Drawing.Point(785, 200);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(571, 177);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -165,9 +143,9 @@ namespace WindowsFormsApp1
             // pictureBox2
             // 
             this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.Screenshot__318_;
-            this.pictureBox2.Location = new System.Drawing.Point(94, 425);
+            this.pictureBox2.Location = new System.Drawing.Point(94, 472);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(571, 171);
+            this.pictureBox2.Size = new System.Drawing.Size(571, 195);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 52;
             this.pictureBox2.TabStop = false;
@@ -175,9 +153,9 @@ namespace WindowsFormsApp1
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Screenshot__317_;
-            this.pictureBox1.Location = new System.Drawing.Point(94, 153);
+            this.pictureBox1.Location = new System.Drawing.Point(94, 200);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(571, 168);
+            this.pictureBox1.Size = new System.Drawing.Size(571, 177);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 51;
             this.pictureBox1.TabStop = false;
@@ -187,7 +165,7 @@ namespace WindowsFormsApp1
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label8.Location = new System.Drawing.Point(558, 9);
+            this.label8.Location = new System.Drawing.Point(528, 17);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(390, 37);
             this.label8.TabIndex = 56;
@@ -219,10 +197,8 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -246,10 +222,8 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
